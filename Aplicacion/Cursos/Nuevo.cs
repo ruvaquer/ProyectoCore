9231ed32f5ec3c05ad265261a31db5b49ca26347
RUBEN\rubva <rubvaquero@gmail.com>
2022-12-31 10:21:31 +0100

    0003 - FUN - Modificación  Class Nuevo

---
+++ Aplicacion/Cursos/Nuevo.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Dominio;
+using MediatR;
+using Persistencia;
+
+namespace Aplicacion.Cursos
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest{
+
+            public string Titulo {get; set;}
+            public string Descripcion {get; set;}
+            public DateTime FechaPublicacion {get; set;}
+        
+        }
+
+        public class Handler : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _context;
+
+            public Handler(CursosOnlineContext context)
+            {
+                _context = context;
+            }
+
+            //Cancelación token se usa cuando un cliente cancela la petición por ejemplo cancelando un peddo por un cliente así se cancela a nivel de back y servidor
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var curso = new Curso{
+                    Titulo = request.Titulo,
+                    Descripcion = request.Descripcion,
+                    FechaPublicacion = request.FechaPublicacion
+                };
+
+                _context.Curso.Add(curso);
+                //Ahora confirmamos la ransacción
+                var valor = await _context.SaveChangesAsync();//Devuelve un estado de la transacción, si es 0 significa que hemos tenido algún error si es 1 o superior es que es correcto.
+                if(valor > 0){
+                    return Unit.Value;
+                }
+
+                //Si falla algo lanzo una alerta de error, solo va ocurrir si no entra en el if anterior
+                throw new Exception("No se pudo insertar el Curso");
+
+            }
+        }
+    }
+}
