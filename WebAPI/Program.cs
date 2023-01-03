f3aff608519910220a21179d876f30b7972c6c23
RUBEN\rubva <rubvaquero@gmail.com>
2023-01-03 20:47:54 +0100

    0003 - FUN - Se ha modificado el proyecto WEBAPI clase Program para dotarla y que sea capaz de crear la migración para crear las tablas desde las clases a la BBDD usando el CursosOnlineContext. Para que se realice esto priemro debemos ponernos en el proyecto WEBAPI en la consila y seguido pulsar el comando dotnet watch run

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistencia;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,27 @@ namespace WebAPI
 {
     public class Program
     {
+        //Modificamos este método para ejecutar nuestro context que esta en persistencia usado para poder ejecutar la migración
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostServer =  CreateHostBuilder(args).Build();
+            //Ahora creo un contexto
+            using(var ambiente = hostServer.Services.CreateScope()){
+                var services = ambiente.ServiceProvider;
+                //Aqui es donde le indico la instancia al CursoOnlineContext
+                var context = services.GetRequiredService<CursosOnlineContext>();
+                //También valido que se ejecute correctamente este procedimiento metiendolo en un try
+                try{
+                    context.Database.Migrate();
+                }catch(Exception ex){
+                  var logging = services.GetRequiredService<ILogger<Program>>();
+                  logging.LogError(ex, "Ocurio un error en la migración.");
+                }
+            }
+
+            hostServer.Run();
+
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
