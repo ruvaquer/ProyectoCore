bf07db6abfbc9d2c186cd3cde893a288461ebc4d
RUBEN\rubva <rubvaquero@gmail.com>
2023-01-03 20:16:29 +0100

    0003 - MOD - He eliminado las tablas de la BBDD para crearlas desde las entidades, he cambiado los id de las claves de cada clase por Guid y he incluido en los decimales su tipo (18,4)

--- Dominio/CursoInstructor.cs
+++ Dominio/CursoInstructor.cs
@@ -1,12 +1,14 @@
+using System;
+
 namespace Dominio
 {
     public class CursoInstructor
     {
-        public int CursoId {get; set;}
+        public Guid CursoId {get; set;}
 
         public Curso Curso {get; set;}
 
-        public int InstructorId {get; set;}
+        public Guid InstructorId {get; set;}
 
         public Instructor Instructor {get; set;}
         
