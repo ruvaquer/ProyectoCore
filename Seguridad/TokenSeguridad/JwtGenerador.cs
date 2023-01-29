7cc4dcbc7381878831072621afd9460971181e78
RUBEN\rubva <rubvaquero@gmail.com>
2023-01-29 10:40:35 +0100

    0003 - FUN - Agregada referencia a System.Identity.Token.Jwt en proyecto Seguridad

--- Seguridad/TokenSeguridad/JwtGenerador.cs
+++ Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Aplicacion.Interfaces;
 using Dominio;
@@ -10,8 +11,17 @@ namespace Seguridad.TokenSeguridad
     //Inyectamos como un servicio dentro del WebAPI, esto lo hacemos dentro de la clase Stastup
     public class JwtGenerador : IJwtGenerador
     {
-        
 
+        /*
+                public string CrearToken(Usuario usuario)
+                {
+                    //1º Lista de Cleans (Dtos dle usuario que queremos compartir con el cliente)
+                    var claims = new List<Claim>{
+                        new Claim(JwtRegisterClaimNames)
+                    };
+
+                }
+                */
         public string CrearToken(Usuario usuario)
         {
             throw new NotImplementedException();
