99edac2892ace99ac92bb7683fe2b3302efa02ee
RUBEN\rubva <rubvaquero@gmail.com>
2023-02-15 18:28:55 +0100

    0003 - FUN - Creadas clases CursoDto e IntructorDTo antes de mapear entre entity y DTO en Consulta ya que reotrnaremos CursoDto que es lo que vrea cliente

---
+++ Aplicacion/Cursos/CursoDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aplicacion.Cursos
+{
+    public class CursoDto
+    {
+        public Guid CursoId {get; set;}
+        public string Titulo {get; set;}
+        public string Descripcion {get; set;}
+        public DateTime? FechaPublicacion {get; set;}
+        public byte[] FotoPortada {get; set;}
+        
+        //CADA CURSO TIENE NA LISTA DE INSTRUCTORES
+        public ICollection<IntructorDto> Instructores {get; set;}
+    }
+}
