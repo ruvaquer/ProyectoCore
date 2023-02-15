afdec7c0da5f3daab9f4897322f2282ed90dbfdf
RUBEN\rubva <rubvaquero@gmail.com>
2023-02-15 18:27:29 +0100

    0003 - FUN - Creados CursosDto e InstructorDTo, en la clase consulta retornaremos CursoDto pero para  realizar la transformación de la entity a DTO usaremos una herramienta de mapeo

--- Aplicacion/Cursos/Consulta.cs
+++ Aplicacion/Cursos/Consulta.cs
@@ -11,11 +11,11 @@ namespace Aplicacion.Cursos
     public class Consulta
     {
         //Esta clase representa la lista de Cursos
-        public class ListaCursos : IRequest<List<Curso>>{}
+        public class ListaCursos : IRequest<List<CursoDto>>{}
 
 
         //Esta otra clase representa la lógica de la operación en si. Handler == Manejador puedes poner también majejador en castellano si te parece más comodo
-        public class Handler : IRequestHandler<ListaCursos, List<Curso>>
+        public class Handler : IRequestHandler<ListaCursos, List<CursoDto>>
         {
             //Propiedad que representa el CursosOnlineContext
             private readonly CursosOnlineContext _context;
@@ -25,9 +25,16 @@ namespace Aplicacion.Cursos
             }
 
             //Aquí es donde importo o consumo al objeto dle entity Frameworck llamando a la BBDD y devolever la lista de cursos (Toda la lógica de negocio)
-            public async Task<List<Curso>> Handle(ListaCursos request, CancellationToken cancellationToken)
+            public async Task<List<CursoDto>> Handle(ListaCursos request, CancellationToken cancellationToken)
             {
-                var cursos = await _context.Curso.ToListAsync();
+                //Así solo devuelvo el curso
+                //var cursos = await _context.Curso.ToListAsync();
+                //Así condigo que me devuelva los datos de los cursos y además los instructores relacionados con el curso
+                var cursos = await _context.Curso
+                .Include(x => x.InstructoresLink)
+                .ThenInclude(x => x.Instructor).ToListAsync();
+                //Pasar el tipo de dato Curso a un tipo CursoDto que es el que mostraremos a Cliente Usaremos una herramienta para mapear clases
+
                 return cursos;
 
             }
