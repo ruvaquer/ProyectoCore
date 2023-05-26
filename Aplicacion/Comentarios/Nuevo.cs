c34e37c3418c904d4ab25c301a744a20c0cd5223
RUBEN\rubva <rubvaquero@gmail.com>
2023-05-26 20:15:16 +0200

    0003 - FUN - Creada alta comentario de un curso con EntityFramework

---
+++ Aplicacion/Comentarios/Nuevo.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dominio;
+using FluentValidation;
+using MediatR;
+using Persistencia;
+
+namespace Aplicacion.Comentarios
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Alumno { get; set; }
+            public int Puntuacion { get; set; }
+            public string Comentario { get; set; }
+            public Guid CursoId { get; set; }
+        }
+
+        public class EjecutaValidacion : AbstractValidator<Ejecuta>{
+            public EjecutaValidacion(){
+                RuleFor(x => x.Alumno).NotEmpty();
+                RuleFor(x => x.Puntuacion).NotEmpty();
+                RuleFor(x => x.Comentario).NotEmpty();
+                RuleFor(x => x.CursoId).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Ejecuta>
+        {
+            private readonly CursosOnlineContext _context;
+
+            public Handler(CursosOnlineContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var comentario = new Comentario(){
+                    ComentarioId = Guid.NewGuid(),
+                    Puntuacion = request.Puntuacion,
+                    Alumno = request.Alumno,
+                    ComentarioTexto = request.Comentario,
+                    CursoId = request.CursoId
+                };
+
+                _context.Comentario.Add(comentario);
+
+                var resultado =  await _context.SaveChangesAsync();
+
+                if(resultado > 0){
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se pudo insertar el comentario");
+            }
+        }
+    }
+}
---
+++ WebAPI/Controllers/ComentarioController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Aplicacion.Comentarios;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    
+    public class ComentarioController : MiControllerBase
+    {
+        private readonly ILogger<ComentarioController> _logger;
+
+        public ComentarioController(ILogger<ComentarioController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Crear(Nuevo.Ejecuta item){
+            return await Mediator.Send(item);
+        }
+    }
+}
